ff8e9840b725713c72d6bf61ab29e5b643b263c5
Nataliya Lisjo <natlisjo@gmail.com>
2020-10-04 00:47:24 +0200

    HomeWork_5

--- ConsoleCore/Program.cs
+++ ConsoleCore/Program.cs
@@ -6,9 +6,10 @@ namespace ConsoleCore
 {
     class Program
     {
-        // connection string till våran device i Azure IotHub
-        private static readonly string _conn = "HostName=ecwin20IoTHub.azure-devices.net;DeviceId=consoleapp;SharedAccessKey=3RSw06VBsoW/NBcIcqQ2tSm6tgWUoNDD+GxRHARsZ78=";
-
+        // connection string till våran device i Azure IotHub 
+        //Name in Device Explorer IoT_upp5
+        private static readonly string _conn = "HostName=ecwin20IoTHub.azure-devices.net;DeviceId=IoT_upp5;SharedAccessKey=eRrd6k4uspN3/JBsiIVizyMgC8I/O1wGaj0isoWS0+E=";
+        private static int telemetryInterval = 5;
 
         // behöver ha (""- connection string till IoT apparat / vi tar från Azure
         // Mqtt- kommunikation protokol mdl=4 kb/ http -stor 
@@ -24,6 +25,7 @@ namespace ConsoleCore
             DeviceService.ReceiveMessageAsync(deviceClient).GetAwaiter();
 
             Console.ReadKey();// låsa ConsoleApp -jag kan se 
+
         }
     }
 }
